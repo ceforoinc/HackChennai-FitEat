ba4b25df4259bccd58709e368f12818f652899c8
Suritam-Nandy <13suritsuritam@gmail.com>
2020-09-19 09:13:54 +0530

    fix issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
-var height = document.getElementById("height");
-var weight = document.getElementById("weight");
-var age = document.getElementById("age");
-var gender = document.getElementById("gender");
-var goalWeight = document.getElementById("goalWeight");
-var days = document.getElementById("days");
+var addData = (ev) => {
+  ev.preventDefault();
+  let userData = {
+    height: document.getElementById("height").value,
+    weight: document.getElementById("weight").value,
+    age: document.getElementById("age").value,
+    gender: document.getElementById("gender").value,
+    goalWeight: document.getElementById("goalWeight").value,
+    days: document.getElementById("days").value,
+  };
+  console.log(userData);
+};
+document.getElementById("btn").addEventListener("click", addData);
