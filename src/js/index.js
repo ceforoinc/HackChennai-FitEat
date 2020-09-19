622c6966cf4002a7f731267afecf9719bff2c102
Suritam-Nandy <13suritsuritam@gmail.com>
2020-09-19 08:26:42 +0530

    fix issues

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var age = document.getElementById("age");
+var gender = document.getElementById("gender");
+var goalWeight = document.getElementById("goalWeight");
+var days = document.getElementById("days");
