d7b28bef4f54399d65482a2871e3bcccf5924d17
Soumik Chaudhuri <pinkman7009@pop-os.localdomain>
2020-09-20 14:44:35 +0530

    landing page added with changes in backend

--- backend/routes/fitness.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router();
-
-const { getFitnessDetails, postFitnessDetails } = require('../controllers/fitness')
-
-router.route('/').get(getFitnessDetails).post(postFitnessDetails);
-
-module.exports = router;
--- backend/routes/meals.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getMealsData, postMealsData } = require('../controllers/meals')
-
-router.route('/').get(getMealsData).post(postMealsData);
-
-module.exports = router;
